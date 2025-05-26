1eeb5ba6beef90b3ed8262633c924e4688bd1322
Neeraj Pandey <neerajpandey025@gmai.com>
2025-05-26 17:00:18 +0530

    Movie/shows list added

--- src/app/services/api/tmdb-api.service.ts
+++ src/app/services/api/tmdb-api.service.ts
@@ -1,10 +1,9 @@
 // src/app/services/tmdb.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment';
-import { Movie, TmdbResponse} from '../../models/tmdb.model';
+import { Movie, TmdbResponse } from '../../models/tmdb.model';
 
 @Injectable({
   providedIn: 'root'
@@ -16,7 +15,7 @@ export class TmdbApiService {
   constructor(private http: HttpClient) { }
 
   getFullImageUrl(path: string | null, size: string = 'w500'): string | null {
-      if (!path) {
+    if (!path) {
       return 'https://via.placeholder.com/500x750.png?text=No+Image';
     }
     return `${this.imageBaseUrl}${size}${path}`;
@@ -29,9 +28,17 @@ export class TmdbApiService {
       .set('region', region)
       .set('sort_by', 'popularity.desc')
       .set('watch_region', region)
-      .set('with_origin_country', region); 
+      .set('with_origin_country', region);
 
     return this.http.get<TmdbResponse<Movie>>(`${this.apiUrl}/discover/movie`, { params });
+  }
+  getPopularTvShows(page: number = 1, language: string = 'en-US'): Observable<TmdbResponse<Movie>> {
+    const params = new HttpParams()
+      .set('language', language)
+      .set('page', page.toString())
+      .set('sort_by', 'popularity.desc')
+
+    return this.http.get<TmdbResponse<Movie>>(`${this.apiUrl}/discover/tv`, { params });
   }
   getTrendingMovies(timeWindow: 'day' | 'week' = 'week', language: string = 'en-US'): Observable<TmdbResponse> {
     const params = new HttpParams()
