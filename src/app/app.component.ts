de6e359ebd93c72b5ba7b2c8048c9c00d75dda3f
Neeraj Pandey <neerajpandey025@gmai.com>
2025-07-26 18:18:28 +0530

    app component sections and calling Genre api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { NavbarComponent } from './components/layout/navbar/navbar.component';
+// import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { FooterComponent } from './components/layout/footer/footer.component';
+import { StoreService } from './services/utils/store.service';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
     CommonModule,
-    NavbarComponent
+    // NavbarComponent,
+    FooterComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MovieDeck';
 
-  constructor() { }
+  constructor(private storeService: StoreService) { }
+
+  ngOnInit(): void {
+    this.storeService.loadGenre();
+  }
 }
