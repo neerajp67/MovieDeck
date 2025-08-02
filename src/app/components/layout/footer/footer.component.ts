6b4e2bcdf92e0937ca970373d1aa9a4d50bc03b3
Neeraj Pandey <neerajpandey025@gmai.com>
2025-08-02 21:46:48 +0530

    Footer component design

--- src/app/components/layout/footer/footer.component.ts
+++ src/app/components/layout/footer/footer.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from "@angular/material/icon";
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [MatToolbarModule],
+  imports: [MatToolbarModule,
+    MatIconModule,
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
