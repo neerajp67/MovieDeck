87341c773168313fa75d39937f4545024fac6e8c
Neeraj Pandey <neerajpandey025@gmai.com>
2025-08-19 21:32:56 +0530

    SOU-18: integrated horizontal component in trending

--- src/app/components/home/trending/trending.component.ts
+++ src/app/components/home/trending/trending.component.ts
@@ -10,6 +10,7 @@ import { FormsModule } from '@angular/forms';
 import { Movie, TmdbResponse } from '../../../models/tmdb.model';
 import { TmdbApiService } from '../../../services/api/tmdb-api.service';
 import { Router } from '@angular/router';
+import { HorizontalScrollComponent } from "../../shared/horizontal-scroll/horizontal-scroll.component";
 
 @Component({
   selector: 'app-trending',
@@ -19,7 +20,8 @@ import { Router } from '@angular/router';
     MatButtonModule,
     MatProgressSpinnerModule,
     MatIconModule,
-    MatButtonToggleModule
+    MatButtonToggleModule,
+    HorizontalScrollComponent
   ],
   templateUrl: './trending.component.html',
   styleUrl: './trending.component.scss',
@@ -33,22 +35,11 @@ export class TrendingComponent implements OnInit, OnDestroy {
   errorMessage: string | null = null;
   activeTimeWindow: TimeWindow = 'day';
 
-  showArrows: boolean = false;
-  canScrollLeft: boolean = false;
-  canScrollRight: boolean = false;
-
-  private isDragging = false;
-  private startX = 0;
-  private currentScrollLeftPosition = 0;
-
-  private mouseMoveSubscription: Subscription | null = null;
-  private mouseUpSubscription: Subscription | null = null;
-
-  private destroy$ = new Subject<void>();
+  private readonly destroy$ = new Subject<void>();
 
   constructor(public movieService: TmdbApiService,
-    private router: Router,
-    private cdr: ChangeDetectorRef
+    private readonly router: Router,
+    private readonly cdr: ChangeDetectorRef
   ) { }
 
   ngOnInit(): void {
@@ -66,15 +57,6 @@ export class TrendingComponent implements OnInit, OnDestroy {
           this.isLoading = false;
           // Using requestAnimationFrame for more reliable DOM layout measurement
           requestAnimationFrame(() => {
-            this.updateScrollArrowVisibility();
-            // Ensure right arrow is initially visible if there are movies
-            if (this.trendingMovies.length > 0 && this.scrollContainer && this.scrollContainer.nativeElement.scrollWidth > this.scrollContainer.nativeElement.clientWidth) {
-              this.canScrollRight = true; // Force right arrow to be true if content is wider
-            } else {
-              this.canScrollRight = false; // Otherwise, if content fits, it's false
-            }
-            this.canScrollLeft = false; // Always hide left arrow initially
-
             this.cdr.detectChanges();
           });
         },
@@ -86,100 +68,6 @@ export class TrendingComponent implements OnInit, OnDestroy {
       });
   }
 
-  onMouseDown(event: MouseEvent): void {
-    if (!this.scrollContainer || event.button !== 0) return;
-
-    this.isDragging = true;
-    this.startX = event.pageX - this.scrollContainer.nativeElement.offsetLeft;
-    this.currentScrollLeftPosition = this.scrollContainer.nativeElement.scrollLeft;
-
-    this.cancelDragSubscriptions();
-
-    this.mouseUpSubscription = fromEvent(document, 'mouseup').subscribe(() => this.onMouseUp());
-    this.mouseMoveSubscription = fromEvent(document, 'mousemove').subscribe((e: Event) => this.onMouseMove(e as MouseEvent));
-
-    event.preventDefault();
-    this.scrollContainer.nativeElement.style.cursor = 'grabbing';
-  }
-
-  onMouseMove(event: MouseEvent): void {
-    if (!this.isDragging || !this.scrollContainer) return;
-
-    event.preventDefault();
-    const x = event.pageX - this.scrollContainer.nativeElement.offsetLeft;
-    const walk = (x - this.startX) * 1.5;
-    this.scrollContainer.nativeElement.scrollLeft = this.currentScrollLeftPosition - walk;
-    this.updateScrollArrowVisibility();
-  }
-
-  onMouseUp(): void {
-    this.isDragging = false;
-    if (this.scrollContainer) {
-      this.scrollContainer.nativeElement.style.cursor = 'grab';
-    }
-    this.cancelDragSubscriptions();
-  }
-
-  private cancelDragSubscriptions(): void {
-    if (this.mouseUpSubscription) {
-      this.mouseUpSubscription.unsubscribe();
-      this.mouseUpSubscription = null;
-    }
-    if (this.mouseMoveSubscription) {
-      this.mouseMoveSubscription.unsubscribe();
-      this.mouseMoveSubscription = null;
-    }
-  }
-
-  onScrollWrapperMouseEnter(): void {
-    this.showArrows = true;
-    this.updateScrollArrowVisibility(); // Recalculate visibility on hover
-    this.cdr.detectChanges();
-  }
-
-  onScrollWrapperMouseLeave(): void {
-    this.showArrows = false;
-    this.cdr.detectChanges();
-  }
-
-  onScroll(): void {
-    this.updateScrollArrowVisibility();
-  }
-
-  private updateScrollArrowVisibility(): void {
-    if (!this?.scrollContainer?.nativeElement) {
-      this.canScrollLeft = false;
-      this.canScrollRight = false;
-      this.cdr.detectChanges();
-      return;
-    }
-
-    const element = this.scrollContainer.nativeElement;
-    const scrollLeft = element.scrollLeft;
-    const scrollWidth = element.scrollWidth;
-    const clientWidth = element.clientWidth;
-
-    this.canScrollLeft = scrollLeft > 0;
-    this.canScrollRight = Math.ceil(scrollLeft + clientWidth) < scrollWidth;
-    this.cdr.detectChanges();
-  }
-
-  scrollContentLeft(): void {
-    if (this.scrollContainer) {
-      const scrollAmount = this.scrollContainer.nativeElement.clientWidth * 0.7;
-      this.scrollContainer.nativeElement.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
-      setTimeout(() => this.updateScrollArrowVisibility(), 600);
-    }
-  }
-
-  scrollRight(): void {
-    if (this.scrollContainer) {
-      const scrollAmount = this.scrollContainer.nativeElement.clientWidth * 0.7;
-      this.scrollContainer.nativeElement.scrollBy({ left: scrollAmount, behavior: 'smooth' });
-      setTimeout(() => this.updateScrollArrowVisibility(), 600);
-    }
-  }
-
   navigateToDetails(item: number) {
     if (item) {
       this.router.navigate(['/', 'movie', item]);
@@ -189,7 +77,6 @@ export class TrendingComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
-    this.cancelDragSubscriptions();
     this.destroy$.next();
     this.destroy$.complete();
   }
