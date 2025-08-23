40ff244966658fd761c46dc5dbb19dfb4cc96ac2
Neeraj Pandey <neerajpandey025@gmai.com>
2025-08-23 21:34:04 +0530

    SOU-36: Cast

--- src/app/components/media-cast/media-cast.component.ts
+++ src/app/components/media-cast/media-cast.component.ts
@@ -1,10 +1,9 @@
-import { Component, ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy, input } from '@angular/core';
+import { Component, ChangeDetectionStrategy, input, signal, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { Subject } from 'rxjs';
 import { TmdbApiService } from '../../services/api/tmdb-api.service';
-import { CastMember } from '../../models/tmdb.model'; // Assuming CastMember interface is defined in a model file
+import { CastMember } from '../../models/tmdb.model';
 import { HorizontalScrollComponent } from '../shared/horizontal-scroll/horizontal-scroll.component';
 
 @Component({
@@ -19,23 +18,15 @@ import { HorizontalScrollComponent } from '../shared/horizontal-scroll/horizonta
   styleUrls: ['./media-cast.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MediaCastComponent implements OnDestroy {
+export class MediaCastComponent {
+  movieService = inject(TmdbApiService);
   cast = input.required<CastMember[]>();
-  placeholderImage: string = 'assets/images/placeholder_person.png';
-
-  private readonly destroy$ = new Subject<void>();
-
-  constructor(public movieService: TmdbApiService, private readonly cdr: ChangeDetectorRef) { }
+  placeholderImage = signal<string>('assets/images/placeholder_person.png');
 
   /**
  * Returns the correct image URL or a placeholder if the profile path is missing.
  */
   getProfileImageUrl(profilePath: string | null): string {
-    return profilePath ? this.movieService.getFullImageUrl(profilePath, 'w185') : this.placeholderImage;
-  }
-
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
+    return profilePath ? this.movieService.getFullImageUrl(profilePath, 'w185') : this.placeholderImage();
   }
 }
