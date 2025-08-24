74c370612d285562c60cfe3427ae4329b497a9cf
Neeraj Pandey <neerajpandey025@gmai.com>
2025-08-24 18:55:31 +0530

    SOU-32: Media card inhancements

--- src/app/components/home/popular/popular.component.ts
+++ src/app/components/home/popular/popular.component.ts
@@ -59,9 +59,11 @@ export class PopularComponent implements OnInit, OnDestroy {
     return {
       id: media.id,
       title: media.title || media.name,
-      poster_path: media.poster_path,
-      release_date: media.release_date || media.first_air_date,
-      vote_average: media.vote_average,
+      subtitle: {
+        poster_path: media.poster_path,
+        release_date: media.release_date || media.first_air_date,
+        vote_average: media.vote_average,
+      },
       media_type: this.selectedCategory()
     };
   }
